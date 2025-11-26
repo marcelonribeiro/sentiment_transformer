fba33a0b521d20e0ff018005e2bd8603b76474f1
marcelo <mnribeiro1985@gmail.com>
2025-11-25 22:11:08 -0300

    correct permissions

--- start.sh
+++ start.sh
@@ -22,11 +22,11 @@ echo ">>> Fixing volume permissions..."
 sleep 5
 
 docker compose exec -u root -T airflow-standalone bash -c "
-    chown -R airflow:0 /app/.dvc/cache &&
+    chown -R airflow:0 /app/.dvc &&
     chown -R airflow:0 /dvc-storage &&
     chown -R airflow:0 /app/mlruns &&
     chown -R airflow:0 /app/airflow &&
-    chmod -R u+w /app/.dvc/cache /dvc-storage /app/mlruns /app/airflow
+    chmod -R u+w /app/.dvc /dvc-storage /app/mlruns /app/airflow
 "
 
 echo ">>> Permissions fixed! System ready."
